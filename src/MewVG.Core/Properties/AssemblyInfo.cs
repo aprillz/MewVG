586c4bf2a730d03ae8986047a83ca8975459c988
Youngjae Song <al6uiz@gmail.com>
2026-07-06 14:08:21 +0900

    Add InternalsVisibleTo for MewVG.Core.Tests

--- src/MewVG.Core/Properties/AssemblyInfo.cs
+++ src/MewVG.Core/Properties/AssemblyInfo.cs
@@ -2,3 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Aprillz.MewVG.Metal")]
 [assembly: InternalsVisibleTo("Aprillz.MewVG.GL")]
+[assembly: InternalsVisibleTo("Aprillz.MewVG.Core.Tests")]
